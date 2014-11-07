3fce0dae59f42b22f0eaefbba33794b4680cba66
Weffy <k.pongsema@gmail.com>
2014-11-06 20:41:16 -0500

    Create number.cpp

---
+++ CSCI127/classwork/number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int m;
+	cout <<"Please enter a number:" << endl;
+	cin >> n;
+	cout <<"Please enter another number: "<< endl;
+	cin >> m;
+	int sum;
+	sum=m+n;
+	cout <<"The sum of " << m << " and " << n << " is: " << sum << endl;
+
+return 0;
+}
