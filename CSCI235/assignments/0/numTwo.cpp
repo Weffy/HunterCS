7f6a5832d9312ba3a3ac3fd45898297d4dc256fb
Weffy <k.pongsema@gmail.com>
2014-11-06 21:34:06 -0500

    Create numTwo.cpp

---
+++ CSCI235/assignments/0/numTwo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int rows = 5;
+	const int columns = 5;
+	int **a;
+
+	a = new int*[rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		a[i] = new int [columns];
+	}
+
+//initialization
+	for (int j = 0; j < columns; j++)
+	{
+		for 
+	}
+	delete a;
+
+	return 0;
+}
